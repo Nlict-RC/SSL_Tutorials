bf5b0cdd3c408470bc7fa1fd8f422892ce1aec5a
Wang <615349118@163.com>
2019-04-11 23:23:27 +0800

    update

--- Python/actionmodule2.py
+++ Python/actionmodule2.py
@@ -5,7 +5,7 @@
 @author: Wang Yunkai
 """
 
-import socket
+import serial
 import struct
 from time import sleep
 
@@ -13,14 +13,8 @@ ACTION_IP = '10.12.225.78'
 ACTION_PORT = 1030
 
 class ActionModule:
-    def __init__(self, ACTION_IP, ACTION_PORT):
-        self.address = (ACTION_IP, ACTION_PORT)
-        self.start_package = b'\xF0\x5A\x5A\x01\x01\xA6'
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.send_start_package()
-    
-    def send_start_package(self):
-        self.socket.sendto(self.start_package, self.address)
+    def __init__(self, COM='COM5'):
+        self.serial = serial.Serial(COM, 115200)
     
     def send_action(self, robot_num=0, vx=0, vy=0, w=0):
         # m/s -> cm/s, rad/s -> 1/40 rad/s
@@ -40,10 +34,13 @@ class ActionModule:
         send_package[17] = struct.pack('!B',int(abs(w)/128) + int(abs(vy)/128)*16 + int(abs(vx)/128)*64)
         package = struct.pack('!25c', send_package[0],send_package[1],send_package[2],send_package[3],send_package[4],send_package[5],send_package[6],send_package[7],send_package[8],send_package[9],send_package[10],send_package[11],send_package[12],send_package[13],send_package[14],send_package[15],send_package[16],send_package[17],send_package[18],send_package[19],send_package[20],send_package[21],send_package[22],send_package[23],send_package[24])
         #print(package)
-        self.socket.sendto(package, self.address)
+        self.serial.write(package)
+        
+    def close(self):
+        self.serial.close()
         
 if __name__ == "__main__":
-    action = ActionModule(ACTION_IP, ACTION_PORT)
+    action = ActionModule('COM5')
     while(True):
-        action.send_action(robot_num=7, vx=0, vy=0, w=1)
+        action.send_action(robot_num=5, vx=0, vy=0, w=3)
         sleep(0.015)
